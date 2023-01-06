318d154f59fc26eb24365899377a5f677b7960b5
Misgana Bayetta <misgana.bayetta@gmail.com>
2023-01-07 00:02:59 +0800

    Test cases

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-use chrono::{Datelike, Utc};
-use std::collections::HashMap;
+use chrono::{DateTime, Datelike, NaiveDate, Utc};
 pub trait Calendar {
     fn jdn(&self) -> u32;
     fn jdn_to_date(&self, jd: u32) -> Date;
@@ -15,11 +14,11 @@ pub struct Date {
 }
 
 pub struct EthiopianCalendar {
-    j_offset: u32,
-    greg_date: Date,
+    pub j_offset: u32,
+    pub greg_date: Date,
 }
 pub struct GregorianCalendar {
-    date: Date,
+    pub date: Date,
 }
 
 impl EthiopianCalendar {
@@ -71,25 +70,27 @@ impl Calendar for EthiopianCalendar {
     }
 
     fn jdn_to_date(&self, jd: u32) -> Date {
-        let r = (jd - self.j_offset) % 1461;
+        let jd = jd as i32;
+        let offset = self.j_offset as i32;
+        let r = (jd - offset) % 1461;
         let n = r % 365 + 365 * r / 1460;
-        let y = 4 * (jd - self.j_offset) / 1461 + r / 365 - r / 1460;
+        let y = 4 * (jd - offset) / 1461 + r / 365 - r / 1460;
         let m = n / 30 + 1;
         let d = n % 30 + 1;
 
         Date {
             month_name: String::from("TODO"),
             day_name: String::from("TODO"),
-            month: m,
-            year: y,
-            day: d,
+            month: m as u32,
+            year: y as u32,
+            day: d as u32,
         }
     }
 }
 
 impl GregorianCalendar {
-    fn new() -> Self {
-        let now = Utc::now();
+    fn new(date: Option<DateTime<Utc>>) -> Self {
+        let now = date.unwrap_or(Utc::now());
         let (_, year) = now.year_ce();
         let date = Date {
             month_name: String::from("TODO"),
@@ -117,20 +118,26 @@ impl Calendar for GregorianCalendar {
             10 => String::from("October"),
             11 => String::from("November"),
             12 => String::from("December"),
-            _ => panic!("Month number cannot exceed 13."),
+            _ => panic!("Month number cannot exceed 12."),
         }
     }
 
     fn jdn(&self) -> u32 {
         // https://www.hermetic.ch/cal_stud/jdn.htm
-        (1461 * (self.date.year + 4800 + (self.date.month - 14) / 12)) / 4
-            + (367 * (self.date.month - 2 - 12 * ((self.date.month - 14) / 12))) / 12
-            - (3 * ((self.date.year + 4900 + (self.date.month - 14) / 12) / 100)) / 4
-            + self.date.day
-            - 32075
+        let m = self.date.month as i32;
+        let y = self.date.year as i32;
+        let d = self.date.day as i32;
+        let r = (1461 * (y + 4800 + (m - 14) / 12)) / 4
+            + (367 * (m - 2 - 12 * ((m - 14) / 12))) / 12
+            - (3 * ((y + 4900 + (d - 14) / 12) / 100)) / 4
+            + d
+            - 32075;
+
+        r as u32
     }
 
     fn jdn_to_date(&self, jd: u32) -> Date {
+        let jd = jd as i32;
         let mut l = jd + 68569;
         let n = (4 * l) / 146097;
         l = l - (146097 * n + 3) / 4;
@@ -145,9 +152,41 @@ impl Calendar for GregorianCalendar {
         Date {
             month_name: String::from("TODO"),
             day_name: String::from("TODO"),
-            month: m,
-            year: y,
-            day: d,
+            month: m as u32,
+            year: y as u32,
+            day: d as u32,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_gregorian_jdn() {
+        let dt = NaiveDate::from_ymd_opt(2023, 1, 6)
+            .unwrap()
+            .and_hms_milli_opt(0, 0, 1, 444)
+            .unwrap()
+            .and_local_timezone(Utc)
+            .unwrap();
+        let greg_cal = GregorianCalendar::new(Some(dt));
+        assert_eq!(greg_cal.jdn(), 2459951);
+    }
+
+    #[test]
+    fn test_greg_jdn_to_eth_cal() {
+        let dt = NaiveDate::from_ymd_opt(2023, 1, 6)
+            .unwrap()
+            .and_hms_milli_opt(0, 0, 1, 444)
+            .unwrap()
+            .and_local_timezone(Utc)
+            .unwrap();
+        let greg_cal = GregorianCalendar::new(Some(dt));
+        let eth_cal = EthiopianCalendar::new();
+        let eth_date = eth_cal.jdn_to_date(greg_cal.jdn());
+        assert_eq!(eth_date.year, 2015);
+        assert_eq!(eth_date.month, 4);
+        assert_eq!(eth_date.day, 5);
     }
 }
